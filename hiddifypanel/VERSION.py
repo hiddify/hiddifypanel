1ec6f5be6b30ee40ecf7b3ac5dc464e4f303016c
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2026-04-28 11:06:34 +0330

    release: version 12.3.0 🚀

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -2,5 +2,5 @@
 from datetime import datetime
 
 __version__ = '12.3.0'
-__release_time__= datetime.strptime('2026-04-28T11:06:07','%Y-%m-%dT%H:%M:%S')
+__release_time__= datetime.strptime('2026-04-28T11:06:29','%Y-%m-%dT%H:%M:%S')
 is_released_version = False
