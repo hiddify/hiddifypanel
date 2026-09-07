845ee3cbc931da18d2dc4dee2b5ebe50edc06ffd
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2026-09-08 01:26:18 +0330

    fix: redis for wiregaurd and telemt

--- hiddifypanel/drivers/ssh_liberty_bridge_api.py
+++ hiddifypanel/drivers/ssh_liberty_bridge_api.py
@@ -13,8 +13,8 @@ class SSHLibertyBridgeApi(DriverABS):
         return hconfig(ConfigEnum.ssh_server_enable)
 
     def get_ssh_redis_client(self):
-        if not hasattr(self, 'redis_client'):
-            self.redis_client = redis.from_url(os.environ.get("REDIS_URI_SSH",""), decode_responses=True)
+        if not hasattr(self, "redis_client"):
+            self.redis_client = redis.from_url(os.environ.get("REDIS_URI_MAIN", ""), decode_responses=True)
 
         return self.redis_client
 
@@ -24,9 +24,9 @@ class SSHLibertyBridgeApi(DriverABS):
         return {m.split("::")[0]: 1 for m in members}
 
     def add_client(self, user):
-        print(f'Adding SSH {user}')
+        print(f"Adding SSH {user}")
         redis_client = self.get_ssh_redis_client()
-        redis_client.sadd(USERS_SET, f'{user.uuid}::{user.ed25519_public_key}')
+        redis_client.sadd(USERS_SET, f"{user.uuid}::{user.ed25519_public_key}")
         redis_client.save()
 
     def remove_client(self, user):
@@ -37,8 +37,8 @@ class SSHLibertyBridgeApi(DriverABS):
                 if member.startswith(user.uuid):
                     redis_client.srem(USERS_SET, member)
 
-        redis_client.srem(USERS_SET, f'{user.uuid}::{user.ed25519_public_key}')
-        redis_client.hdel(USERS_USAGE, f'{user.uuid}')
+        redis_client.srem(USERS_SET, f"{user.uuid}::{user.ed25519_public_key}")
+        redis_client.hdel(USERS_USAGE, f"{user.uuid}")
         redis_client.save()
 
     def get_all_usage(self):
@@ -62,5 +62,5 @@ class SSHLibertyBridgeApi(DriverABS):
             redis_client.hincrby(USERS_USAGE, client_uuid, -value)
             redis_client.save()
         if value:
-            logger.debug(f'ssh usage {client_uuid} {value}')
+            logger.debug(f"ssh usage {client_uuid} {value}")
         return value
--- hiddifypanel/drivers/telemt_api.py
+++ hiddifypanel/drivers/telemt_api.py
@@ -15,35 +15,37 @@ USERS_USAGE = "tele:users-usage"
 
 class TelemtApi(DriverABS):
     def get_redis_client(self):
-        if not hasattr(self, 'redis_client'):
-            self.redis_client = redis.from_url(os.environ.get("REDIS_URI_SSH",""))
+        if not hasattr(self, "redis_client"):
+            self.redis_client = redis.from_url(os.environ.get("REDIS_URI_MAIN", ""))
 
         return self.redis_client
 
     def is_enabled(self) -> bool:
-        return hconfig(ConfigEnum.telegram_enable) and hconfig(ConfigEnum.telegram_lib)=="telemt"
+        return hconfig(ConfigEnum.telegram_enable) and hconfig(ConfigEnum.telegram_lib) == "telemt"
 
     def __init__(self) -> None:
         super().__init__()
-        self.tg_uuid_map:dict[str,str]={}
+        self.tg_uuid_map: dict[str, str] = {}
 
     def __load_tg_uuid_map(self):
         from hiddifypanel.database import db
-        users = db.session.query(User).all()
-        self.tg_uuid_map={u.uuid.replace("-",""): u.uuid for u in users}
 
-    def __convert_tg_to_uuid(self,pubkeys):
-        res={}
-        can_reload_map=True
+        users = db.session.query(User).all()
+        self.tg_uuid_map = {u.uuid.replace("-", ""): u.uuid for u in users}
+
+    def __convert_tg_to_uuid(self, pubkeys):
+        res = {}
+        can_reload_map = True
         for key in pubkeys:
-            if uuid:=self.tg_uuid_map.get(key):
-                res[key]=uuid
+            if uuid := self.tg_uuid_map.get(key):
+                res[key] = uuid
             elif can_reload_map:
                 self.__load_tg_uuid_map()
-                can_reload_map=False
-                if uuid:=self.tg_uuid_map.get(key):
-                    res[key]=uuid
+                can_reload_map = False
+                if uuid := self.tg_uuid_map.get(key):
+                    res[key] = uuid
         return res
+
     def get_metric(self):
         resp = requests.get("http://localhost:10087/metrics", timeout=5)
         resp.raise_for_status()
@@ -57,9 +59,7 @@ class TelemtApi(DriverABS):
         # telemt_user_octets_from_client{user="uuid"} 1983
         # telemt_user_octets_to_client{user="uuid"} 2171
 
-        pattern = re.compile(
-            r'telemt_user_octets_(from_client|to_client)\{user="([^"]+)"\}\s+(\d+)'
-        )
+        pattern = re.compile(r'telemt_user_octets_(from_client|to_client)\{user="([^"]+)"\}\s+(\d+)')
 
         for line in raw_output.splitlines():
             match = pattern.search(line)
@@ -80,7 +80,7 @@ class TelemtApi(DriverABS):
         return data
 
     def __get_local_usage(self) -> dict:
-        usage_data = self.get_redis_client() .get(USERS_USAGE)
+        usage_data = self.get_redis_client().get(USERS_USAGE)
         if usage_data:
             return json.loads(usage_data)
 
@@ -89,22 +89,21 @@ class TelemtApi(DriverABS):
     def __sync_local_usages(self) -> dict:
         local_usage = self.__get_local_usage()
         tg_usage = self.__get_tg_usages()
-        
+
         res = {}
         # remove local usage that is removed from wg usage
         for local_uuid in local_usage.copy().keys():
             if local_uuid not in tg_usage:
                 del local_usage[local_uuid]
 
-        
         uuid_map = self.__convert_tg_to_uuid(tg_usage.keys())
         for tg_uuid, tg_usage in tg_usage.items():
             uuid = uuid_map.get(tg_uuid)
-            
+
             if not local_usage.get(tg_uuid):
                 local_usage[tg_uuid] = {"uuid": uuid, "usage": tg_usage}
                 continue
-            res[uuid] = self.calculate_reset(local_usage[tg_uuid]['usage'], tg_usage)
+            res[uuid] = self.calculate_reset(local_usage[tg_uuid]["usage"], tg_usage)
             local_usage[tg_uuid] = {"uuid": uuid, "usage": tg_usage}
 
         self.get_redis_client().set(USERS_USAGE, json.dumps(local_usage))
@@ -113,14 +112,14 @@ class TelemtApi(DriverABS):
 
     def calculate_reset(self, last_usage: dict, current_usage: dict) -> dict:
         res = {
-            'up': current_usage['up'] - last_usage['up'],
-            'down': current_usage['down'] - last_usage['down'],
+            "up": current_usage["up"] - last_usage["up"],
+            "down": current_usage["down"] - last_usage["down"],
         }
 
-        if res['up'] < 0:
-            res['up'] = 0
-        if res['down'] < 0:
-            res['down'] = 0
+        if res["up"] < 0:
+            res["up"] = 0
+        if res["down"] < 0:
+            res["down"] = 0
         return res
 
     def get_enabled_users(self):
@@ -130,7 +129,7 @@ class TelemtApi(DriverABS):
         new_wg_pubs = set(usages.keys())
         old_usages = self.__get_local_usage()
         old_wg_pubs = set(old_usages.keys())
-        enabled = {u['uuid']: 1 for u in old_usages.values()}
+        enabled = {u["uuid"]: 1 for u in old_usages.values()}
         not_included = new_wg_pubs - old_wg_pubs
         if not_included:
             users = User.query.filter(User.wg_pub.in_(not_included).all())
@@ -150,9 +149,9 @@ class TelemtApi(DriverABS):
             return {}
         all_usages = self.__sync_local_usages()
         res = {}
-        for uuid,use in all_usages.items():
+        for uuid, use in all_usages.items():
             # if use := all_usages.get(u.wg_pub):
-                res[uuid] = use['up'] + use['down']
-            # else:
-            #     res[u] = 0
+            res[uuid] = use["up"] + use["down"]
+        # else:
+        #     res[u] = 0
         return res
